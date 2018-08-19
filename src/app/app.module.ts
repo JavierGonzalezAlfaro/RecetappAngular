9574b276ab35eab2d9a3745414beea0cb7502062
Javier Gonzalez <javichu.12@gmail.com>
2018-08-19 21:13:20 +0100

    basic setup angular con componentes iniciales y bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    CardDetailComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
